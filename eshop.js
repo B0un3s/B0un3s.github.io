01dcea320725c85f534650cc1ff46f832cb69681
B0un3s <144420550+B0un3s@users.noreply.github.com>
2025-05-15 19:41:11 +0200

    Delete eshop.js

--- eshop.js
+++
@@ -1,8 +0,0 @@
-// eshop.js
-// Hide out-of-stock products
-// Add data-in-stock="false" to any .service-card div to mark it as unavailable
-
-document.addEventListener('DOMContentLoaded', () => {
-  const outOfStockCards = document.querySelectorAll('.service-card[data-in-stock="false"]');
-  outOfStockCards.forEach(card => card.remove());
-});
